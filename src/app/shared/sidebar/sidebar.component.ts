4a06a8a9ed5e461b113ed74bb2ffed375b3753a2
HumanWeapon <odair.umanzor@unah.hn>
2023-11-20 23:37:48 -0600

    carga de objetos

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,9 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Objetos } from 'src/app/interfaces/seguridad/objetos';
+import { ErrorService } from 'src/app/services/error.service';
+import { ObjetosService } from 'src/app/services/seguridad/objetos.service';
 import { SidebarService } from 'src/app/services/sidebar.service';
 
 @Component({
@@ -8,24 +12,41 @@ import { SidebarService } from 'src/app/services/sidebar.service';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit{
-  menuItems:any[];
+  menuItems:any[]=[];
   userName: string = '';
+  listObjetos: Objetos [] = [];
 
-  constructor(private _sideBarService: SidebarService, private router:Router){
-    this.menuItems = this._sideBarService.menu;
+  constructor(private _sideBarService: SidebarService, 
+    private router:Router,
+    private _objetos: ObjetosService,
+    private _errorService: ErrorService){
     //this.menuItems2 = this.sideBarService.empresas;
   }
 
   ngOnInit(): void {
+    this.getObjetos();
+    this.menuItems = this._sideBarService.menu;
+    /*
     this.menuItems = this._sideBarService.menu;
     const local = localStorage.getItem('usuario');
     if(local !== null){
       this.userName = local;
-    };
+    };*/
   }
 
   logout(){
     this.router.navigateByUrl('/login');
     localStorage.clear();
+  }
+  getObjetos(){
+    this._objetos.getAllObjetos().subscribe({
+      next: (data: any) => {
+        console.log(data);
+        this.listObjetos = data;
+      },
+      error: (e: HttpErrorResponse) => {
+        this._errorService.msjError(e);
+      }
+    });
   }
 }
