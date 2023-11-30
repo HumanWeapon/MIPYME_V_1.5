07402ee55772185a4bad80f69b575e785c3dad67
imsamv <ismael.midence@unah.hn>
2023-11-29 21:15:05 -0600

    Imagen Perfil

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -7,6 +7,8 @@ import { UsuariosService } from 'src/app/services/seguridad/usuarios.service';
 import { ToastrService } from 'ngx-toastr';
 import { Roles } from 'src/app/interfaces/seguridad/roles';
 import { RolesService } from 'src/app/services/seguridad/roles.service';
+import { DomSanitizer } from '@angular/platform-browser';
+import { error } from 'jquery';
 
 
 
@@ -26,6 +28,11 @@ export class PerfilComponent  implements OnInit{
   inputDeshabilitado: boolean = true; // input Deshabilitado/bloqueado
   botonDeshabilitado: boolean = true;
   mostrarBoton: boolean = false; //Oculta el Boton de Cancelar
+  mostrarCamposContrasena = false;
+  mostrarEditarUsuario = true;
+  mostrarBotonGuardar: boolean = false;
+  public imagenes: any = [];
+  public previsualizacion: string = '';
 /********************************************************************************************** */
 
  listRoles: Roles [] = [];
@@ -67,7 +74,7 @@ export class PerfilComponent  implements OnInit{
     private _userService: UsuariosService,
     private toastr: ToastrService,
     private _rolService: RolesService,
-    
+    private sanitizer: DomSanitizer
     ){}
 
   ngOnInit(): void {
@@ -102,7 +109,9 @@ export class PerfilComponent  implements OnInit{
 
   //Bloqueo y Desbloqueo de Inputs
 habilitarInput() {
+  this.mostrarEditarUsuario = false;
   this.inputDeshabilitado = false;
+  this.mostrarCamposContrasena = true; // Mostrar campos al habilitar la edición
 }
 
 habilitarInputPassword() {
@@ -116,6 +125,8 @@ deshabilitarInput() {
 
 //Metodo de Ocultar/Mostrar Boton
 cancelarInput(){
+this.mostrarEditarUsuario = true;
+this.mostrarCamposContrasena = false; // Ocultar campos al cancelar la edición
 this.mostrarBoton=false;
 this.inputDeshabilitado = true;
 this.botonDeshabilitado=true;
@@ -145,47 +156,128 @@ this.botonDeshabilitado = false;
 }
 //Fin Metodo de Ocultar/Mostrar Boton
 
-editarUsuario() {
-  this._userService.editarUsuario(this.usuario).subscribe(data => {
-    this.toastr.success('Usuario editado con éxito');
-    this.usuario.nombre_usuario = this.usuario.nombre_usuario;
-    this.usuario.correo_electronico = this.usuario.correo_electronico;
-  });
-this.mostrarBoton=false;
-this.botonDeshabilitado = true;
-this.inputDeshabilitado = true;
-}
-
-validarPassword() {
-  // Obtener la contraseña almacenada en el Local Storage
-  const userLocal = localStorage.getItem('CCP')
-  if (this.contrasenaActual === '' || this.nuevaContrasena === '' || this.confirmarContrasena === '') {
+validarCambios() {
+  if (this.usuario.usuario === '' || this.usuario.nombre_usuario === '' || this.usuario.correo_electronico === '') {
     this.toastr.warning('Completa todos los campos');
-  } else if (this.confirmarContrasena !== this.nuevaContrasena) {
-    this.toastr.warning('Las contraseñas no coinciden');
-  } else if (userLocal === null) {
-    this.toastr.warning('No se encontró una contraseña almacenada en el Local Storage');
-  } else if (this.contrasenaActual !== userLocal) {
-    this.toastr.warning('La contraseña actual no coincide con la contraseña almacenada');
   } else {
-    // Contraseñas coinciden, procede a cambiar la contraseña
-    this.usuario.contrasena = this.nuevaContrasena; // Asigna la nueva contraseña
-    this._userService.cambiarContrasena(this.usuario).subscribe((data) => {
+    // Realiza la actualización de usuario, nombre y correo
+    this._userService.editarUsuario(this.usuario).subscribe((data) => {
       if (data) {
-        this.toastr.success('Contraseña actualizada con éxito', 'success');
+        this.toastr.success('Cambios guardados con éxito', 'success');
+        this.mostrarBoton = false; // Oculta el botón Cancelar
+        this.mostrarEditarUsuario = true; // Muestra el botón Editar Usuario
+        this.mostrarCamposContrasena = false; // Oculta los campos de contraseña
+        this.botonDeshabilitado = true; // Vuelve a deshabilitar el botón de Guardar Cambios
+        this.inputDeshabilitado = true; // Vuelve a deshabilitar los campos de edición
       } else {
-        this.toastr.error('Error al actualizar la contraseña', 'error');
+        this.toastr.error('Error al guardar los cambios', 'error');
       }
     });
   }
 
-  // Restablece los campos y deshabilita el botón después de procesar
+  // Restablece los campos después de procesar
   this.contrasenaActual = '';
   this.nuevaContrasena = '';
   this.confirmarContrasena = '';
-  this.mostrarBoton = false;
-  this.botonDeshabilitado = true;
-  this.inputDeshabilitado = true;
+}
+
+validarPassword() {
+  // Obtener la contraseña almacenada en el Local Storage
+  const userLocal = localStorage.getItem('CCP');
+
+  if (this.contrasenaActual === '') {
+    // Contraseña actual no proporcionada, realizar la actualización de usuario, nombre y correo
+    this.validarCambios();
+  } else {
+    // Contraseña actual proporcionada, validar y cambiar la contraseña
+    if (this.nuevaContrasena === '' || this.confirmarContrasena === '') {
+      this.toastr.warning('Completa todos los campos de contraseña');
+    } else if (this.confirmarContrasena !== this.nuevaContrasena) {
+      this.toastr.warning('Las contraseñas no coinciden');
+    } else if (userLocal === null) {
+      this.toastr.warning('No se encontró una contraseña almacenada en el Local Storage');
+    } else if (this.contrasenaActual !== userLocal) {
+      this.toastr.warning('La contraseña actual no coincide con la contraseña almacenada');
+    } else {
+      // Contraseñas coinciden, procede a cambiar la contraseña
+      this.usuario.contrasena = this.nuevaContrasena; // Asigna la nueva contraseña
+      this._userService.cambiarContrasena(this.usuario).subscribe((data) => {
+        if (data) {
+          this.toastr.success('Contraseña actualizada con éxito', 'success');
+          this.mostrarBoton = false; // Oculta el botón Cancelar
+          this.mostrarEditarUsuario = true; // Muestra el botón Editar Usuario
+          this.mostrarCamposContrasena = false; // Oculta los campos de contraseña
+          this.botonDeshabilitado = true; // Vuelve a deshabilitar el botón de Guardar Cambios
+          this.inputDeshabilitado = true; // Vuelve a deshabilitar los campos de edición
+        } else {
+          this.toastr.error('Error al actualizar la contraseña', 'error');
+        }
+      });
+    }
+  }
+
+  // Restablece los campos después de procesar
+  this.contrasenaActual = '';
+  this.nuevaContrasena = '';
+  this.confirmarContrasena = '';
+}
+
+imagenPerfil(event: Event): any {
+  // Verificar que event.target no sea nulo
+  if (event.target) {
+    const imagenCapturada = (event.target as HTMLInputElement).files?.[0];
+
+    // Verificar que se haya seleccionado al menos un archivo
+    if (imagenCapturada) {
+      this.extraerBase64(imagenCapturada).then((imagen: any) => {
+        this.previsualizacion = imagen.base;
+        console.log(imagen);
+
+        // Mostrar el botón "Guardar" después de cambiar la imagen
+        this.mostrarBotonGuardar = true;
+      });
+      this.imagenes.push(imagenCapturada);
+    } else {
+      console.error('No se seleccionó ningún archivo.');
+    }
+  } else {
+    console.error('El evento no tiene un objetivo (target).');
+  }
+}
+
+
+
+extraerBase64 = async ($event: any) => {
+  try {
+    const unsafeImg = window.URL.createObjectURL($event);
+    const imagen = this.sanitizer.bypassSecurityTrustUrl(unsafeImg);
+    const reader = new FileReader();
+
+    return new Promise((resolve, reject) => {
+      reader.readAsDataURL($event);
+      reader.onload = () => {
+        resolve({
+          blob: $event,
+          imagen,
+          base: reader.result
+        });
+      };
+      reader.onerror = error => {
+        resolve({
+          blob: $event,
+          imagen,
+          base: null
+        });
+      };
+    });
+  } catch (e) {
+    return null;
+  }
+};
+
+guardarImagen(){
+
+  
 }
 
 
