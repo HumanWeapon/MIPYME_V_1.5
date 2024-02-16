0f3c1ac3d9a3a37b895b8ecdc91d3bed6de9ed85
HumanWeapon <odair.umanzor@unah.hn>
2024-02-15 23:53:55 -0600

    correcciones en tabla de productos

--- src/app/interfaces/mantenimiento/productos.ts
+++ src/app/interfaces/mantenimiento/productos.ts
@@ -1,8 +1,6 @@
 export interface Productos {
     id_producto: number,
     id_categoria: number,
-    id_pais: number,
-    id_contacto: number,
     producto: string,
     descripcion: string,
     creado_por: string,
