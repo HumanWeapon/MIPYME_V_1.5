3844ae0ac02bcd6e1e4159b61549d1b05c6d706a
HumanWeapon <odair.umanzor@unah.hn>
2024-04-06 00:28:12 -0600

    cambios para corregir ciertos bugs

--- src/app/pages/pyme/pyme.component.ts
+++ src/app/pages/pyme/pyme.component.ts
@@ -84,12 +84,12 @@ export class PymeComponent {
   ngOnInit(): void {
  
  
-  this.dtOptions = {
-    pagingType: 'full_numbers',
-    pageLength: 10,
-    language: { url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json' },
-    responsive: true,
-  };
+    this.dtOptions = {
+      pagingType: 'full_numbers',
+      pageLength: 10,
+      language: { url: '//cdn.datatables.net/plug-ins/1.13.6/i18n/es-ES.json' },
+      responsive: true,
+    };
     this._pymesService.getAllPymes()
     .subscribe({
       next: (data) =>{
