e7471cbcd1e721f5ad2d31a96b6f18b737569b97
HumanWeapon <odair.umanzor@unah.hn>
2024-04-17 22:21:56 -0600

    cambios en el routesGuard

--- src/app/utils/routes.guard.ts
+++ src/app/utils/routes.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable, catchError, map, of } from 'rxjs';
 import { PermisosService } from '../services/seguridad/permisos.service';
@@ -8,34 +8,52 @@ import { PermisosService } from '../services/seguridad/permisos.service';
   providedIn: 'root'
 })
 
-export class PermisosGuard implements CanActivate {
-  constructor(private _permisosService: PermisosService, private router: Router) {}
+export class PermisosGuard implements CanActivate{
+  id_rol: any = ''
+  id_objeto: any = ''
+  id_objeto2: any = ''
+  id_usuario: any = ''
+
+  constructor(private _permisosService: PermisosService, private router: Router) {
+    // Obtener los identificadores de roles, objeto y usuario del localStorage
+    this.id_rol = localStorage.getItem('id_rol');
+    this.id_objeto = localStorage.getItem('id_objeto');
+    this.id_objeto2 = this.id_objeto;
+    this.id_usuario = localStorage.getItem('id_usuario');
+  }
 
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    // Obtener los identificadores de roles, objeto y usuario del localStorage
-    const id_rol = localStorage.getItem('id_rol');
-    const id_objeto = localStorage.getItem('id_objeto');
-    const id_usuario = localStorage.getItem('id_usuario');
+    
+
+    
+
 
     //console.log(id_rol);
     //console.log(id_objeto);
     //console.log(id_usuario);
     // Verificar si todos los identificadores están disponibles
-    if(id_rol && id_objeto && id_usuario) {
 
-      return this._permisosService.permisosdeRoutes(id_rol, id_objeto, id_usuario).pipe(
+    console.log(this.id_rol);
+    console.log(this.id_objeto);
+    console.log(this.id_usuario);
+
+    if(this.id_rol && this.id_objeto && this.id_usuario) {
+
+      return this._permisosService.permisosdeRoutes(this.id_rol, this.id_objeto2, this.id_usuario).pipe(
         map(permisos => {
-          if (permisos.length > 0) {
+          if (permisos) {
+            console.log(permisos)
             // Si el usuario tiene permisos, permite el acceso a la ruta
-            localStorage.removeItem('id_objeto');
             return true;
           } else {
             // Si el usuario no tiene permisos, redirige a una página de acceso denegado
+            
             return this.router.createUrlTree(['/dashboard/access-denied']);
           }
+          
         }),
         catchError(() => {
           this.router.navigate(['/dashboard/not-found']);
