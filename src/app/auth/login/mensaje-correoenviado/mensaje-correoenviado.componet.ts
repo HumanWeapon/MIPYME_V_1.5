c1bc0e4d6b780c6befd35eb6862176b83c8d9d1b
imsamv <ismael.midence@unah.hn>
2024-05-06 20:48:53 -0600

    Msj antes de ir al enlace del correo

--- src/app/auth/login/mensaje-correoenviado/mensaje-correoenviado.componet.ts
+++ src/app/auth/login/mensaje-correoenviado/mensaje-correoenviado.componet.ts
@@ -15,12 +15,43 @@ import { UsuariosService } from 'src/app/services/seguridad/usuarios.service';
 export class MensajeCorreoEnviadoComponent implements OnInit {
   
   constructor(private router: Router, 
-
+     private userService: UsuariosService
      ) { }
+
+     usuario: Usuario = {
+      id_usuario: 0,
+      creado_por: '',
+      fecha_creacion: new Date(),
+      modificado_por: '',
+      fecha_modificacion: new Date(),
+      usuario: '',
+      nombre_usuario: '',
+      correo_electronico: '',
+      estado_usuario: 0,
+      contrasena: '',
+      id_rol: 0,
+      fecha_ultima_conexion: new Date(),
+      fecha_vencimiento: new Date(),
+      intentos_fallidos: 0
+    };
+
   
   ngOnInit(): void {
-
+      this.getUsuario()
   }
+
+  getUsuario(){
+    const userLocal = localStorage.getItem('usuario');
+    if(userLocal == null){
+
+    }else{
+      this.usuario.usuario = userLocal;
+      this.userService.getUsuario(this.usuario).subscribe(data => {
+        this.usuario = data;
+      });
+    }
+  }
+  
 
   cerrarModal() {
     this.router.navigate(['/login']);
