7eb4f4c7162a2d686e2e69c5d7eb5203c287c175
HumanWeapon <odair.umanzor@unah.hn>
2024-05-07 22:14:52 -0600

    cambios en requisitos

--- src/enviroments/enviromet.ts
+++ src/enviroments/enviromet.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
     production: true,
-    //endpoint: 'https://angularnodeapi-production.up.railway.app/'
-    endpoint: 'http://localhost:3001/'
+    endpoint: 'https://angularnodeapi-production.up.railway.app/'
+    //endpoint: 'http://localhost:3001/'
   };
   
   /*
